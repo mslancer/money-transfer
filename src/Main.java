1ee64a5442425eef1bd1518b132bec0594fcaa2d
mslancer <wolfnw@yandex.ru>
2021-07-15 22:50:09 +0500

    Initial

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int bank = 2_000_000_000;
+        int transfer = 500_000_000;
+        int total = bank + transfer;
+        System.out.println(total);
+    }
+}
+
